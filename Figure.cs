011539025b74af9c66c190bab181d85184746f20
isHardCoded <ishardcoded@gmail.com>
2025-12-14 16:00:58 +0500

    Создание класса, get/set, конструктор, ToString()

---
+++ Figure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Figure
+    {
+        protected int width;
+        protected int height;
+
+        public int Width
+        {
+            get => width;
+            set => width = value;
+        }
+
+        public Figure(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"Width: {width}, Height: {height}";
+        }
+    }
+}
