23bb822f2c59a0b0a76fc7084fa785641165254d
isHardCoded <ishardcoded@gmail.com>
2025-12-20 15:47:49 +0500

    Написали абстрактный класс Servce

---
+++ Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal abstract class Service
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int ResponseTime { get; set; }
+
+        public Service(string name, string status, int responseTime)
+        {
+            Name = name;
+            Status = status;
+            ResponseTime = responseTime;
+        }
+
+        public abstract void GetStatus();
+    }
+}
