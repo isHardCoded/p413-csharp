59aa75a2990a7352d44c0aefe989364b45889836
isHardCoded <ishardcoded@gmail.com>
2025-12-20 17:39:32 +0500

    Работа с файловой системой

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -11,32 +12,23 @@ namespace ConsoleApp26
     {
         static void Main(string[] args)
         {
-            List<Service> services = new List<Service>();
-            Dictionary<string, Service> serviceDictionary = new Dictionary<string, Service>();
-
-            WebServer web1 = new WebServer("Web1", "Online", 120, "top-academy.ru");
-            Database db1 = new Database("DB1", "Offline", 250, "MySQL");
-
-            services.Add(web1);
-            services.Add(db1);
-
-            serviceDictionary[web1.Name] = web1;
-            serviceDictionary[db1.Name] = db1;
-
-            foreach (var service in services)
+            string filePath = @"C:\Users\User\Desktop\data.txt";
+         
+            using (StreamWriter writer = new StreamWriter(filePath, append: false, encoding: Encoding.UTF8))
             {
-                service.GetStatus();
+                writer.WriteLine("Hello");
+                writer.WriteLine("Hello");
+                writer.WriteLine("Hello");
             }
 
-            Console.WriteLine("\nВведите имя сервиса для проверки статуса: ");
-            string input = Console.ReadLine();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
 
-            if (serviceDictionary.ContainsKey(input))
-            {
-                serviceDictionary[input].GetStatus();
-            } else
-            {
-                Console.WriteLine("Сервис не найден");
+                while((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
             }
         }
     }
