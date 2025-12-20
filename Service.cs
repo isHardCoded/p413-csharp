33bb8aecef64587274d556ca0601bd3eea1c3d98
isHardCoded <ishardcoded@gmail.com>
2025-12-20 16:05:32 +0500

    Реализовали WebServer и Database

--- Service.cs
+++ Service.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp26
 {
-    internal abstract class Service
+    public abstract class Service
     {
         public string Name { get; set; }
         public string Status { get; set; }
@@ -20,5 +20,34 @@ namespace ConsoleApp26
         }
 
         public abstract void GetStatus();
+    }
+
+    public class WebServer : Service
+    {
+        public string Domain { get; set; }
+
+        public WebServer(string name, string status, int responseTime, string domain) : base(name, status, responseTime) {
+            Domain = domain;
+        }
+
+        public override void GetStatus()
+        {
+            Console.WriteLine($"[Веб-сервер] {Name} ({Domain}): Статус = {Status}, Время отклика = {ResponseTime} мс");
+        }
+    }
+
+    public class Database : Service
+    {
+        public string DbType { get; set; }
+
+        public Database(string name, string status, int responseTime, string dbType) : base(name, status, responseTime)
+        {
+            DbType = dbType;
+        }
+
+        public override void GetStatus()
+        {
+            Console.WriteLine($"[База данных] {Name} ({DbType}): Статус = {Status}, Время отклика = {ResponseTime} мс");
+        }
     }
 }
