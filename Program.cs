f3aaaaf8a4f1558392b44c8e06ecb37abbea292f
isHardCoded <ishardcoded@gmail.com>
2025-12-21 16:36:52 +0500

    Реализовали StudentManager

--- Program.cs
+++ Program.cs
@@ -7,29 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp26
-{
+{ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Users\User\Desktop\data.txt";
-         
-            using (StreamWriter writer = new StreamWriter(filePath, append: false, encoding: Encoding.UTF8))
-            {
-                writer.WriteLine("Hello");
-                writer.WriteLine("Hello");
-                writer.WriteLine("Hello");
-            }
-
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                string line;
-
-                while((line = reader.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-            }
+             
         }
     }
 }
